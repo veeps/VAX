29bc711411e8eb67299d855191b02e6982afd775
Vivian Peng <vpeng12@gmail.com>
2014-02-12 15:51:16 -0500

    data clean up for Whooping Cough

--- VAXpreventableOutbreaks.R
+++ VAXpreventableOutbreaks.R
@@ -9,3 +9,32 @@ head(USoutbreaks)
 # select for Whooping Cough cases
 WhoopingCough <- USoutbreaks[grep("Whooping Cough", USoutbreaks$Outbreak), ]
 head(WhoopingCough)
+
+# separate States from location column
+require(plyr)
+require(stringr)
+location <- str_split_fixed(WhoopingCough$Location, " ",2)
+colnames(location) <-c("Country", "State")
+location<- as.data.frame(location)
+citystate <- str_split_fixed(uspop.c2$City, ", ", 2)
+
+# remove parenthesis from State column
+a <- gsub("\\(", "", location$State)
+State <- gsub("\\)", "", a)
+
+# split city into its own column
+City <- str_split_fixed(State, ", ",2)
+colnames(City) <- c("State", "City")
+Cities <- as.data.frame(City)
+
+# add city and state columns back into Whooping Cough table
+WhoopingCough$State <- Cities$State
+WhoopingCough$City <- Cities$City
+WhoopingCough <- WhoopingCough[,-3]
+head(WhoopingCough)
+
+
+
+
+
+
