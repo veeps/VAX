5c60a916e2ceb546503f23c2d3cd219a7ed5d4e7
Vivian Peng <vpeng12@gmail.com>
2014-02-12 16:02:45 -0500

    initial mapping in R

--- VAXpreventableOutbreaks.R
+++ VAXpreventableOutbreaks.R
@@ -31,10 +31,27 @@ Cities <- as.data.frame(City)
 WhoopingCough$State <- Cities$State
 WhoopingCough$City <- Cities$City
 WhoopingCough <- WhoopingCough[,-3]
-head(WhoopingCough)
+
+# Change numeric variables to numerics instead of characters
+WhoopingCough$Long <- as.numeric(WhoopingCough$Long)
+WhoopingCough$Lat <- as.numeric(WhoopingCough$Lat)
+WhoopingCough$Cases <- as.numeric(WhoopingCough$Cases)
+WhoopingCough$Fatalities <- as.numeric(WhoopingCough$Fatalities)
+colnames(WhoopingCough) <- c("Category", "Outbreak", "Lat", "Long", "Date", "Cases", "Fatalities", "ImpactScale", "SourceCitation", "Source", "State", "City")
+names(WhoopingCough)
 
 
+##########################################################
+require(maps)
+require(maptools)
+require(ggplot2)
+# find max and min longitude and latitude
+long <- c(min(WhoopingCough$Long),max(WhoopingCough$Long) )
+lat <- c(min(WhoopingCough$Lat),max(WhoopingCough$Lat) )
 
-
+all_states <- map_data("state")
+MapCases <- ggplot(data=WhoopingCough, aes(Long,Lat))
+MapCases + geom_polygon(data=all_states, aes(x=long, y=lat, group=group, color=State), colour="darkgray", fill = "ivory") +
+  geom_point(aes(size=Cases)) + xlim(long) + ylim(lat)
 
 
