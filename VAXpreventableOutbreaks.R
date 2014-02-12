7e500fab488fa8c034016c11e525297686ae442f
Vivian Peng <vpeng12@gmail.com>
2014-02-12 15:38:16 -0500

    import csv data into R

--- VAXpreventableOutbreaks.R
+++ VAXpreventableOutbreaks.R
@@ -1,2 +1,11 @@
 setwd("/Users/vivianpeng/git/VAX")
 outbreaks <- read.csv("VAXpreventableOubtreaks.csv", header = TRUE, stringsAsFactors = FALSE, sep =",")
+names(outbreaks)
+# look for US cases
+USoutbreaks <- outbreaks[grep("U.S.", outbreaks$Location),]
+
+head(USoutbreaks)
+
+# select for Whooping Cough cases
+WhoopingCough <- USoutbreaks[grep("Whooping Cough", USoutbreaks$Outbreak), ]
+head(WhoopingCough)
